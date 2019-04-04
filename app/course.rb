33b0b696f0125d0c7a11a02adeb4a53f90b7c65c
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-04-04 09:44:31 -0400

    doneeeeee

--- app/course.rb
+++ app/course.rb
@@ -2,8 +2,30 @@
 
 class Course
   attr_accessor :subject
+  @@all = []
 
   def initialize(subject)
     @subject = subject
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_subject(subject)
+    @@all.find { |item| item.subject == subject}
+  end
+
+  def self.all_subjects
+  end
+
+  def enrollments
+    Enrollment.all.select {|enroll| enroll.course == self}
+  end
+
+  def class_list
+    enrollments.map {|students| students.student.full_name}
+  end
+
 end
